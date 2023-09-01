a669a2ced0d6fe514f93b35a68579fdc5408ad38
Zoren Villareal <zoren.villareal@gmail.com>
2023-08-31 18:27:17 -0700

    got server database connected to supabase

---
+++ server/migrations/20230830042235-create-restaurant.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Restaurants', {
+
+      Restaurant_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      cuisine: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      year_founded: {
+        type: Sequelize.SMALLINT,
+        allowNull:false
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Restaurants');
+  }
+};
---
+++ server/server.js
@@ -0,0 +1,17 @@
+//dependencies
+const express = require('express')
+const app = express()
+const { Sequelize } = require('sequelize')
+const path = require('path');
+const cors = require('cors');
+
+
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//LISTEN
+app.listen(4005,() => {
+    console.log('Server is running on port 4005')
+})
