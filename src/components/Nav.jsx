0185ad07504bc52106034d69e2f165827d47bf8f
Jennifer <jennifermdevine88@gmail.com>
2023-08-31 20:20:37 -0700

    styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
+import { Link, Route, Routes } from 'react-router-dom'
 
 function Nav() {
     return (
       <div className="header">
         <Navbar>
           <Container>
-            <Navbar>In-Or-Out?</Navbar>
+            <Navbar><Link className="home" to='/'>In-Or-Out?</Link></Navbar>
           </Container>
+          <Routes>
+            <Route path='/'></Route>
+          </Routes >
         </Navbar>
       </div>
     );
